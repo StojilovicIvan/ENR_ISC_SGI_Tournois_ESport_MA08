889050de83b12ac640b753e9c3c5be92dc6dbac1
StojilovicIvan <61650191+StojilovicIvan@users.noreply.github.com>
2021-01-23 17:27:43 +0100

    Création des SELECT

---
+++ RENDU/SelectDatabase_ENR_ISC_SGI.sql
@@ -0,0 +1,31 @@
+SELECT * FROM Teams;
+
+SELECT * FROM Players;
+
+SELECT * FROM Coachs;
+
+SELECT * FROM Train;
+
+SELECT * FROM Organizations;
+
+SELECT * FROM Sponsors;
+
+SELECT * FROM Sponsor;
+
+SELECT * FROM Tournaments;
+
+SELECT * FROM Organize;
+
+SELECT * FROM Levels;
+
+SELECT * FROM Matches;
+
+SELECT * FROM Commentators;
+
+SELECT * FROM Broadcasts;
+
+SELECT * FROM Broadcast;
+
+SELECT * FROM Platforms;
+
+SELECT * FROM Games;
